64659b4019a64efe0f72fe83354d05ec23108c51
Josh Wright <jwwrightcalpoly@gmail.com>
2022-08-26 08:40:26 -0600

    simplified condition name

--- libs/shared/util-ick/src/lib/ick.ts
+++ libs/shared/util-ick/src/lib/ick.ts
@@ -1,7 +1,7 @@
 export enum Condition {
   withIck = 'With I Cannot Know',
   withoutIck = 'Without I Cannot Know',
-  combined = 'Combined (Without ICK followed by With ICK)'
+  combined = 'Combined'
 }
 
 export const CONDITIONS = Object.values(Condition);
